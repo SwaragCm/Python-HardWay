760468257d72f6d9425b3ae4c35963688024f569
SwaragCm <swaragcm@gmail.com>
2024-05-23 12:45:31 +0530

    Backslash - Inserting horizontal whitespace using \t - Here this line will split with the help of \n

---
+++ exe10.py
@@ -0,0 +1,13 @@
+tabby_cat="\t i'm tabbed in"            #inserting horizontal whitespace using \t
+persian_cat="i'm split\non a line"      #here this line will split with the help of \n
+backslash_cat="I'm \\ a \\ cat"
+fat_cat="""
+i will do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
