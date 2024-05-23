05dbed5bcbeb76ce67cfcb91f7ea6f50476bd0fe
SwaragCm <swaragcm@gmail.com>
2024-05-23 14:09:49 +0530

    Prompting People - Put a prompt inside a input(), so that other people know what to type.

---
+++ exe12.py
@@ -0,0 +1,6 @@
+# On input(), Iv'e put in a prompt to show to a person so he knows what to type 
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
