48a687243e7234843b717e5707308dc3a40fcd56
SwaragCm <swaragcm@gmail.com>
2024-05-23 13:50:35 +0530

    Input - Accepting input from user using input() - Print out those values using format string

---
+++ exe11.py
@@ -0,0 +1,10 @@
+print("How old are you?", end=' ')
+#Get values from user using input()
+age=input()
+print("How tall are you?",end=' ')
+height=input()
+print("How much do you weight?",end=" ")
+weight=input()
+
+# print those values using format string
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
