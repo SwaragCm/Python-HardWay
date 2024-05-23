d2daa3e95868102a252d5b6e75d98719125b1227
SwaragCm <swaragcm@gmail.com>
2024-05-23 12:18:00 +0530

    Printing, Printing, Printing - Used \n to print the string on a new line - """ use triple quotes to create strings that span multiple lines without having to use escape characters like "\n" for line breaks.

---
+++ exe9.py
@@ -0,0 +1,18 @@
+# here i'm assigning  a string which has all days mentioned .
+days="Mon Tue Wed Thu Fri Sat Sun"
+# assigned some months to a variable  
+months="Jan\nFeb\nmar\napr\nmay\njun\njul\naug"
+
+#this will print the values in a single line
+print("here are the days:",days)
+
+#print each months on a new line, i used "\n" to do this.
+print("here are the months:",months)
+
+#triple quotes to create strings that span multiple lines without having to use escape characters like "\n" for line breaks.
+print("""
+there's something going on her.
+with the three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want ,or 5, or 6.
+""")
