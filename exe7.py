f74a0096ec06235c2d1d252d15546e267104048a
SwaragCm <swaragcm@gmail.com>
2024-05-23 10:03:36 +0530

    More Printing - Practiced more on printing - Used asterisk(*) to print values multiple times - print different characters in a single line using (end=" ")

---
+++ exe7.py
@@ -0,0 +1,23 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format("snow"))  #adding another string to an existing string
+print("And everywhere that Mary went.")
+print("."*10) # this will print 10 dots , here * is helping to do that, this will multiply string 
+
+end1="C"
+end2="h"
+end3="e"
+end4="e"
+end5="s"
+end6="e"
+end7="B"
+end8="u"
+end9="r"
+end10="g"
+end11="e"
+end12="r"
+
+# watch that comma at the end. try  removing it to see what happens
+
+ # adding each characters and print that in a single line with the help of (end=" ")
+print(end1 + end2 + end3 + end4 + end5 + end6, end=" ")
+print(end7 + end8 + end9 + end10 + end11+ end12)
